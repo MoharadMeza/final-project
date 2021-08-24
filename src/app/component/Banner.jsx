9159651447989e8d279230aa71524220b1adb97c
mohawwadrez <mohawwadrez@yahoo.com>
2021-08-24 14:08:50 +0430

    banner new style added

--- src/app/component/Banner.jsx
+++ src/app/component/Banner.jsx
@@ -8,39 +8,41 @@ const Banner = () => {
             <div className="container h-100">
                 <div className="row h-100 justify-content-end">
                     <div className="col-xl-6  col-sm-12 col-md-12 col1">
-                    {/* <h4 className="textTitle p-2 ">
+                        {/* <h4 className="textTitle p-2 ">
                             شرکت توانیر
                         </h4> */}
-                        <p className="texbanner p-3">
+                        {/* <p className="texbanner p-3">
                             در صورت تمایل به ثبت درخواست جدید، به بخش ثبت درخواست و در صورت تمایل به پیگیری درخواست قبلی ، با در دست داشتن کد رهگیری درخواست خود، به بخش پیگیری درخواست مراجعه نمایید.
-                        </p>
+                        </p> */}
                     </div>
                     <div className=" col-xl-6  col-sm-12 col-md-12 col2">
                         <div className="row">
-                        
-                        <div className=" pt-5 p-2 p-sm-unset ">
-                            <Link to="/ticket/add">
-                                <button className="btn btn-danger rounded-pill w-100 btnbaner">
-                                    اعلام خسارت
-                                </button>
-                            </Link>
+                            <p className="texbanner p-2 pt-xl-5 px-3" >
+                                در صورت تمایل به ثبت درخواست جدید، به بخش ثبت درخواست و در صورت تمایل به پیگیری درخواست قبلی ، با در دست داشتن کد رهگیری درخواست خود، به بخش پیگیری درخواست مراجعه نمایید.
+                            </p>
+                            <div className="  p-2 btn1  ">
+                                <Link to="/ticket/add" className="w-100 d-flex justify-content-center btnlink">
+                                    <button className="btn btn-danger rounded-pill w-75 btnbaner">
+                                        اعلام خسارت
+                                    </button>
+                                </Link>
+                            </div>
+                            <div className=" p-2 btn1">
+                                <Link to="ticket/detail" className="w-100 d-flex justify-content-center btnlink">
+                                    <button className="btn btn-danger rounded-pill w-75 btnbaner">
+                                        پیگیری خسارت
+                                    </button>
+                                </Link>
+                            </div>
+                            <div className=" p-2 btn1">
+                                <Link to="ticket/detail" className="w-100 d-flex justify-content-center btnlink">
+                                    <button className="btn  btn-danger rounded-pill w-75 btnbaner">
+                                        راهنمای کاربر
+                                    </button>
+                                </Link>
+                            </div>
                         </div>
-                        <div className=" p-2 ">
-                            <Link to="ticket/detail">
-                                <button className="btn btn-danger rounded-pill w-100 btnbaner">
-                                    پیگیری خسارت
-                                </button>
-                            </Link>
-                        </div>
-                        <div className=" p-2 ">
-                            <Link to="ticket/detail">
-                                <button className="btn  btn-danger rounded-pill w-100 btnbaner">
-                                    راهنمای کاربر
-                                </button>
-                            </Link>
-                        </div>
-                        </div>
-                        
+
                     </div>
                 </div>
             </div>
