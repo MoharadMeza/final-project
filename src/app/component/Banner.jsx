611210ba3c0ce967a301ebc2c2983f658c80782b
mohawwadrez <mohawwadrez@yahoo.com>
2021-08-24 13:48:36 +0430

    banner style fixed

--- src/app/component/Banner.jsx
+++ src/app/component/Banner.jsx
@@ -7,37 +7,40 @@ const Banner = () => {
         <section className="banner">
             <div className="container h-100">
                 <div className="row h-100 justify-content-end">
-                    <div className="col-6 align-self-center">
-                        <h4 className="textTitle p-2 px-5">
-                     شرکت توانیر
-                        </h4>
-                        <p className="texbanner p-2 px-5">
+                    <div className="col-xl-6  col-sm-12 col-md-12 col1">
+                    {/* <h4 className="textTitle p-2 ">
+                            شرکت توانیر
+                        </h4> */}
+                        <p className="texbanner p-3">
                             در صورت تمایل به ثبت درخواست جدید، به بخش ثبت درخواست و در صورت تمایل به پیگیری درخواست قبلی ، با در دست داشتن کد رهگیری درخواست خود، به بخش پیگیری درخواست مراجعه نمایید.
                         </p>
-                        <div className="row p-2 px-5">
+                    </div>
+                    <div className=" col-xl-6  col-sm-12 col-md-12 col2">
+                        <div className="row">
+                        
+                        <div className=" pt-5 p-2 p-sm-unset ">
                             <Link to="/ticket/add">
                                 <button className="btn btn-danger rounded-pill w-100 btnbaner">
                                     اعلام خسارت
                                 </button>
                             </Link>
-
                         </div>
-                        <div className="row p-2 px-5">
+                        <div className=" p-2 ">
                             <Link to="ticket/detail">
                                 <button className="btn btn-danger rounded-pill w-100 btnbaner">
                                     پیگیری خسارت
                                 </button>
                             </Link>
-
                         </div>
-                        <div className="row p-2 px-5">
-                        <Link to="ticket/detail">
-                        <button className="btn  btn-danger rounded-pill w-100 btnbaner">
-                                راهنمای کاربر
-                            </button>
+                        <div className=" p-2 ">
+                            <Link to="ticket/detail">
+                                <button className="btn  btn-danger rounded-pill w-100 btnbaner">
+                                    راهنمای کاربر
+                                </button>
                             </Link>
-                            
                         </div>
+                        </div>
+                        
                     </div>
                 </div>
             </div>
